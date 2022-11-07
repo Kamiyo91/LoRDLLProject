0716175787d3301c8a157df3519c71842a77f2b8
Kamiyo <kamiyo.k91@gmail.com>
2022-11-07 18:50:13 +0100

    Added Random Draw Specific Pages Passive

---
+++ Buffs/BattleDiceCardBuf_TempCard_DLL4221.cs
@@ -0,0 +1,6 @@
+﻿namespace BigDLL4221.Buffs
+{
+    public class BattleDiceCardBuf_TempCard_DLL4221 : BattleDiceCardBuf
+    {
+    }
+}
---
+++ Passives/PassiveAbility_DrawSpecialCards_DLL4221.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using BigDLL4221.Buffs;
+using HarmonyLib;
+
+namespace BigDLL4221.Passives
+{
+    public class PassiveAbility_DrawSpecialCards_DLL4221 : PassiveAbilityBase
+    {
+        public List<LorId> CardIds;
+
+        public void SetCards(List<LorId> cardsIds)
+        {
+            CardIds = cardsIds;
+        }
+
+        public override void OnRoundStartAfter()
+        {
+            var cardNumber = RandomUtil.SelectOne(CardIds);
+            var card = owner.allyCardDetail.AddNewCard(cardNumber);
+            card.AddBuf(new BattleDiceCardBuf_TempCard_DLL4221());
+        }
+
+        public override void OnUseCard(BattlePlayingCardDataInUnitModel curCard)
+        {
+            if (curCard.card.HasBuf<BattleDiceCardBuf_TempCard_DLL4221>())
+                owner.allyCardDetail.ExhaustACardAnywhere(curCard.card);
+        }
+
+        public override void OnRoundEndTheLast()
+        {
+            owner.allyCardDetail.GetAllDeck()
+                .Where(x => x.HasBuf<BattleDiceCardBuf_TempCard_DLL4221>())
+                .Do(x => owner.allyCardDetail.ExhaustACardAnywhere(x));
+        }
+    }
+}
